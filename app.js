c0dac25dacdf9c7929ce3f90b34ea23aa0838fbd
Karrui Lau <karrui.lau@gmail.com>
2018-02-28 12:54:10 +0000

    Update landing UI

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(expressValidator());
 app.use(flash());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
 
 // =====================================
 // DATABASE SETUP ======================
