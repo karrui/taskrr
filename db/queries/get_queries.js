9a7365c80070d9c2807170bdd3ad2cb181f275b5
Nguyen Thanh <thanhson16198@gmail.com>
2018-03-03 16:26:18 +0800

    Added Create queries and edited format of Get queries

--- db/queries/get_queries.js
+++ db/queries/get_queries.js
@@ -2,21 +2,44 @@
 // imagine all calls to these methods to be "get.[REFERENCE]",
 // i.e: get.ALL_CATEGORIES
 
-
-exports.ALL_CATEGORIES =
-    'SELECT * FROM category ' +
-    'ORDER BY id';
-    
-exports.ALL_TASKS =
-    'SELECT * FROM task ' +
-    'ORDER BY start_dt';
-
-exports.USER_BY_ID =
-    'SELECT * ' +
-    'FROM person ' +
-    'WHERE id = $1';
-
-exports.USER_BY_NAME = 
-    'SELECT * ' +
-    'FROM person ' +
-    'WHERE username = $1';
+exports.ALL_CATEGORIES = `
+SELECT
+    category.id,
+    category.name
+FROM category
+ORDER BY
+    category.id
+;
+`
+exports.ALL_TASKS = `
+SELECT
+    task.id,
+    task.title,
+    task.description,
+    task.category_id,
+    task.location,
+    task.requester,
+    task.start_dt,
+    task.end_dt,
+    task.price,
+    task.status_task,
+    task.assignee
+FROM task
+ORDER BY
+    task.start_dt
+;
+`
+exports.USER_BY_ID = `
+SELECT *
+FROM person
+WHERE 1=1
+    AND id = $1
+;
+`
+exports.USER_BY_NAME = `
+SELECT *
+FROM person
+WHERE 1=1
+    AND username = $1
+;
+`
