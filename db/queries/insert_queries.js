91b628b53ca79d35bc922fee4676be310d00dd71
Nguyen Thanh <thanhson16198@gmail.com>
2018-03-06 16:39:42 +0800

    Add insert offer function

--- db/queries/insert_queries.js
+++ db/queries/insert_queries.js
@@ -4,14 +4,42 @@
 
 // Calling Functions
 
+/* Input:
+_title TEXT,
+_description TEXT,
+_category_id INTEGER,
+_location TEXT,
+_requester VARCHAR(25),
+_start_dt TIMESTAMP,
+_end_dt TIMESTAMP,
+_price MONEY
+*/
 exports.ONE_TASK = `
     SELECT
         insert_one_task($1, $2, $3, $4, $5, $6, $7, $8)
     ;
 `
 
+/* Input:
+_username VARCHAR(25),
+_password CHAR(100),
+_email TEXT,
+_created_dt TIMESTAMP
+*/
 exports.ONE_PERSON = `
     SELECT
         insert_one_person($1, $2, $3, $4)
+    ;
+`
+
+/* Input:
+_task_id INTEGER,
+_price MONEY,
+_assignee VARCHAR(25),
+_offered_dt TIMESTAMP
+*/
+exports.ONE_OFFER = `
+    SELECT
+        insert_one_offer($1, $2, $3, $4)
     ;
 `
