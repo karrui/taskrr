87bd811db0b2c219af4edea89b09f855d21ab8e4
Kar Rui Lau <e0032073@u.nus.edu>
2018-03-06 15:08:01 +0800

    Pass 'loggedIn' var to res.locals to remove need to pass as parameter

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use(passport.session()); // persistent login sessions
 
 app.use(function(req,res,next){
     res.locals.currentUser = req.user;
+    res.locals.loggedIn = loggedIn;
     next();
 })
 
@@ -69,7 +70,7 @@ executer.createAllIndexes();
 // executer.deletePopulatedTasks();
 
 app.get("/", function(req, res) {
-    res.render("landing", {loggedIn: loggedIn});
+    res.render("landing");
 });
 
 // =====================================
@@ -91,7 +92,7 @@ function isLoggedIn(req, res, next) {
 // show the login form
 app.get('/login', function(req, res) {
     // render the page and pass in any flash data if it exists
-    res.render('login', { message: req.flash('loginMessage'), loggedIn: loggedIn });
+    res.render('login', { message: req.flash('loginMessage') });
 });
 
 // process the login form
@@ -119,7 +120,7 @@ function login_validation(req, res, next){
 
 
 app.get("/signup", function(req, res) {
-    res.render('signup', { loggedIn: loggedIn, message: req.flash('signupMessage') });
+    res.render('signup', { message: req.flash('signupMessage') });
 })
 
 // process the signup form
@@ -190,10 +191,7 @@ app.get("/tasks/", function(req, res) {
     var promise = executer.getAllTasks();
     promise.then(results => {
         var tasks = results.rows;
-        res.render("tasks", {
-            loggedIn: loggedIn, 
-            tasks: tasks
-        });
+        res.render("tasks", { tasks: tasks });
     });
 });
 
@@ -201,7 +199,7 @@ app.get("/tasks/new", function(req, res) {
     var promise = executer.getCategories();
     promise.then(results => {
         var categories = results.rows;
-        res.render("new", {categories: categories, loggedIn: loggedIn});
+        res.render("new", { categories: categories });
     });
 });
 
@@ -214,7 +212,7 @@ app.get("/tasks/:id", function(req, res) {
     var promise = executer.getTaskById(req.params['id']);
     promise.then(results => {
         var task = results.rows[0];
-        res.render("task_page", {loggedIn: loggedIn, task: task});
+        res.render("task_page", { task: task });
     });
 })
 
@@ -239,7 +237,7 @@ app.get("/categories", function(req, res) {
     var promise = executer.getCategories();
     promise.then(results => {
         var categories = results.rows;
-        res.render("categories", {categories: categories, loggedIn: loggedIn});
+        res.render("categories", { categories: categories });
     });
 });
 
@@ -247,13 +245,13 @@ app.get("/categories/:id", function(req, res) {
     var promise = executer.getTasksByCategoryId(req.params['id']);
     promise.then(results => {
         var tasks = results.rows;
-        res.render("tasks", {loggedIn: loggedIn, tasks: tasks});
+        res.render("tasks", { tasks: tasks });
     });
 });
 
 //404
 app.use(function(req, res, next){
-    res.status(404).render('404', {title: "Sorry, page not found", loggedIn: loggedIn});
+    res.status(404).render('404', { title: "Sorry, page not found" });
 });
 
 // BEFORE YOU START THIS SERVER RUN IN NEW TERMINAL TAB TO ESTABLISH LINK
