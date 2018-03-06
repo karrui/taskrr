03e4930b85d22c1631ea8f422314c89e2c70a31f
Nguyen Thanh <thanhson16198@gmail.com>
2018-03-07 00:33:54 +0800

    Change Update queries to functions and edit format of queries

--- db/queries/update_queries.js
+++ db/queries/update_queries.js
@@ -1,14 +1,11 @@
 exports.OFFER_BY_ASSIGNEE_AND_TASKID = `
-    UPDATE offer
-    SET price = $3, offered_dt = $4
-    WHERE 1=1
-        AND assignee = $1
-        AND task_id = $2
+    SELECT
+        update_offer_by_assignee_taskid($1, $2, $3, $4)
+    ;
 `
 
 exports.TASK_BY_ID =`
-    UPDATE task
-    SET title = $2, description = $3, category_id = $4, location = $5, start_dt = $6, end_dt = $7, price = $8
-    WHERE 1=1
-        AND id = $1
+    SELECT
+        update_task_by_id($1, $2, $3, $4, $5, $6, $7, $8)
+    ;
 `
